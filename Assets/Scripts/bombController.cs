caf87a6a605ba9cf1a6f9917f4585b11e4b3ca35
JuanHidalgoB <juan.hidalgo@uao.edu.co>
2022-11-16 21:56:57 -0500

    Modificacion de la escena y corroborando logica de las esferas

--- Assets/Scripts/bombController.cs
+++ Assets/Scripts/bombController.cs
@@ -1,22 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class bombController : MonoBehaviour
 {
     public float fallSpeed = 1f;
     public string colorBomb = "";
+    int contador;
+    public Text puntos;
+    public TextMeshProUGUI puntos2;
 
     // Start is called before the first frame update
     void Start()
     {
-;
+        contador = 0;
     }
     
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.down * fallSpeed * Time.deltaTime);
+        puntos2.text = "puntos: " + contador.ToString();
     }
     void OnCollisionEnter(Collision other)
     {
@@ -25,15 +31,19 @@ public class bombController : MonoBehaviour
         if (colorBomb == "yellow" && other.gameObject.name == "collisionYellow")
         {
             Debug.Log("Bomba amarilla anotada en contenedor amarillo.");
+            contador++;
             Destroy(gameObject);
         } else if (colorBomb == "blue" && other.gameObject.name == "collisionBlue")
         {
             Debug.Log("Bomba azul anotada en contenedor azul.");
+            contador++;
             Destroy(gameObject);
+            
         }
         else if (colorBomb == "red" && other.gameObject.name == "collisionRed")
         {
             Debug.Log("Bomba roja anotada en contenedor roja.");
+            contador++;
             Destroy(gameObject);
         }else if(other.gameObject.tag == "Floor")
         {
