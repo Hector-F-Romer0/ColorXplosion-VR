8e9aa0a0a61c6167c410508aa7f88b27301ee2c5
JuanHidalgoB <juan.hidalgo@uao.edu.co>
2022-11-18 01:45:06 -0500

    Temporizador cuenta regresiva con gameOver

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public int min, seg;
+    public Text texto;
+
+    public Image gameOver;
+
+    int intervalo = 1;
+    float nextTime = 0;
+
+    private void Start() {
+        gameOver.enabled=false;
+    }
+    private void Update() {
+
+        if(Time.time >= nextTime){
+
+            nextTime+= intervalo;
+            seg-=1;
+        }
+
+        if(seg<0){
+            seg = 0;
+        }
+
+        if(seg==0 & min >= 1){
+            min-=1;
+            seg=59;
+        }
+
+        if(seg==0 & seg == 0){
+
+            gameOver.enabled=true;
+        }
+
+        texto.text = min.ToString("00")+":"+seg.ToString("00");
+    
+    }
+    
+}
